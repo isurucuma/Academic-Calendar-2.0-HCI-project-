80f8431cca9bc02ac6b25fa41a226a4e50e00be1
isuru <isurucuma@gmail.com>
2022-11-29 22:14:30 +0530

    theme whitch enabled

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { ThemeSwitch } from "./micro_components/components";
 
-type Props = {};
+type Props = {
+  setIsDark: (isDark: boolean) => void;
+  isDark: boolean;
+};
 
-function Header({}: Props) {
+function Header({ setIsDark, isDark }: Props) {
   return (
-    <div className="h-20 p-4 items-center bg-slate-300 flex flex-row justify-between text-gray-700">
+    <div className="h-20 p-4 w-full items-center bg-slate-300 dark:bg-slate-900 flex flex-row justify-between text-gray-700 dark:text-gray-200">
       <h1 className="text-lg">Academic Calendar</h1>
       <p className="text-sm">Home/Academic Calendar</p>
+      <ThemeSwitch
+        className="absolute top-3 right-4"
+        onClick={() => {
+          setIsDark(!isDark);
+          console.log("isDark: ", isDark);
+        }}
+      />
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
 import Content from "../components/Content";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function Home() {
+  const [isDark, setIsDark] = useState<boolean>(false);
   return (
-    <>
-      <Header />
-
+    <div className={isDark ? "dark" : ""}>
+      <Header {...{ setIsDark, isDark }} />
       <Content />
       <Footer />
-    </>
+    </div>
   );
 }

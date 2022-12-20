c58345c295f146b0f54d50a43ad62172eef74cda
Hiran Eranda <76918310+hiranEranda@users.noreply.github.com>
2022-12-20 20:18:07 +0530

    dates hard coded for convnce

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { addYears } from "date-fns";
+import { getMonth, getDate } from "date-fns";
 
 import Filters from "./Filters";
 import DialogSelect from "./DropDownRes";
@@ -7,58 +7,51 @@ import WeekSummery from "./WeekSummery";
 
 type Props = {};
 const weeks = [
-  { label: "week 1" },
-  { label: "week 2" },
-  { label: "week 3" },
-  { label: "week 4" },
-  { label: "week 5" },
-  { label: "week 6" },
-  { label: "week 7" },
-  { label: "Mid exam" },
-  { label: "week 9" },
-  { label: "week 10" },
-  { label: "week 11" },
-  { label: "week 12" },
-  { label: "week 13" },
-  { label: "week 14" },
-  { label: "Study leave" },
-  { label: "Examination" },
-  { label: "Vacation" },
-  { label: "week 1" },
-  { label: "week 2" },
-  { label: "week 3" },
-  { label: "week 4" },
-  { label: "week 5" },
-  { label: "week 6" },
-  { label: "week 7" },
-  { label: "Mid exam" },
-  { label: "week 9" },
-  { label: "week 10" },
-  { label: "week 11" },
-  { label: "week 12" },
-  { label: "week 13" },
-  { label: "week 14" },
-  { label: "Study leave" },
-  { label: "Examination" },
-  { label: "Vacation" },
-  { label: "week 1" },
-  { label: "week 2" },
-  { label: "week 3" },
-  { label: "week 4" },
-  { label: "week 5" },
-  { label: "week 6" },
-  { label: "week 7" },
-  { label: "Mid exam" },
-  { label: "week 9" },
-  { label: "week 10" },
-  { label: "week 11" },
-  { label: "week 12" },
-  { label: "week 13" },
-  { label: "week 14" },
-  { label: "Study leave" },
-  { label: "Examination" },
-  { label: "Vacation" },
+  { label: "week 1", startDate: "2023-01-02", endDate: "2023-01-06" },
+  { label: "week 2", startDate: "2023-01-09", endDate: "2023-01-13" },
+  { label: "week 3", startDate: "2023-01-16", endDate: "2023-01-20" },
+  { label: "week 4", startDate: "2023-01-23", endDate: "2023-01-27" },
+  { label: "week 5", startDate: "2023-01-30", endDate: "2023-02-03" },
+
+  { label: "week 6", startDate: "2023-02-06", endDate: "2023-02-10" },
+  { label: "week 7", startDate: "2023-02-13", endDate: "2023-02-17" },
+  { label: "Mid exam", startDate: "2023-02-20", endDate: "2023-02-24" },
+  { label: "week 9", startDate: "2023-02-27", endDate: "2023-03-03" },
+
+  { label: "week 10", startDate: "2023-03-06", endDate: "2023-03-10" },
+  { label: "week 11", startDate: "2023-03-13", endDate: "2023-03-17" },
+  { label: "week 12", startDate: "2023-03-20", endDate: "2023-03-27" },
+  { label: "week 13", startDate: "2023-03-27", endDate: "2023-03-31" },
+
+  { label: "week 14", startDate: "2023-04-03", endDate: "2023-04-07" },
+  { label: "Study leave", startDate: "2023-04-10", endDate: "2023-04-14" },
+  { label: "Examination", startDate: "2023-04-17", endDate: "2023-04-21" },
+  { label: "Vacation", startDate: "2023-04-24", endDate: "2023-04-28" },
+
+  { label: "week 1", startDate: "2023-05-01", endDate: "2023-05-05" },
+  { label: "week 2", startDate: "2023-05-08", endDate: "2023-05-12" },
+  { label: "week 3", startDate: "2023-05-15", endDate: "2023-05-19" },
+  { label: "week 4", startDate: "2023-05-22", endDate: "2023-05-26" },
+
+  { label: "week 5", startDate: "2023-05-29", endDate: "2023-06-02" },
+  { label: "week 6", startDate: "2023-06-05", endDate: "2023-06-09" },
+  { label: "week 7", startDate: "2023-06-12", endDate: "2023-06-16" },
+  { label: "Mid exam", startDate: "2023-06-19", endDate: "2023-06-23" },
+
+  { label: "week 9", startDate: "2023-06-26", endDate: "2023-06-30" },
+  { label: "week 10", startDate: "2023-07-03", endDate: "2023-07-07" },
+  { label: "week 11", startDate: "2023-07-10", endDate: "2023-07-14" },
+  { label: "week 12", startDate: "2023-07-17", endDate: "2023-07-21" },
+
+  { label: "week 13", startDate: "2023-07-24", endDate: "2023-08-04" },
+  { label: "week 14", startDate: "2023-07-31", endDate: "2023-08-11" },
+  { label: "Study leave", startDate: "2023-08-07", endDate: "2023-08-11" },
+  { label: "Examination", startDate: "2023-08-14", endDate: "2023-08-18" },
+
+  { label: "Vacation", startDate: "2023-08-21", endDate: "2023-08-25" },
 ];
+
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
 function Calendar({}: Props) {
   const [clicked, setClicked] = useState(false);
@@ -69,11 +62,19 @@ function Calendar({}: Props) {
     return classes.filter(Boolean).join(" ");
   }
 
-  // const date = new Date();
-  // console.log(date);
+  function getDayFormat(date: string) {
+    // console.log(getDay(new Date(date)));
+    let dayPrefix;
+    if (getDate(new Date(date)) === 1) {
+      dayPrefix = "st";
+    } else if (getDate(new Date(date)) === 2) {
+      dayPrefix = "nd";
+    } else dayPrefix = "th";
+    return `${getDate(new Date(date))}${dayPrefix}  ${monthNames[getMonth(new Date(date))]}`;
+  }
 
   // const year = addYears(date, 1);
-  // console.log(year);
+  console.log(getDate(new Date("2023-01-09")));
 
   // function getWeeksDiff(startDate, endDate) {
   //   const msInWeek = 1000 * 60 * 60 * 24 * 7;
@@ -116,11 +117,12 @@ function Calendar({}: Props) {
       <div className="grid grid-cols-6 gap-1 sm:gap-2 md:gap-4 max-w-[1224px]">
         {weeks.map((val, i) => (
           <>
+            {/* {console.log(val.startDate, val.endDate)} */}
             <div className="flex items-center h-8 text-sm bg-gray-200 border border-gray-300 rounded-md dark:text-gray-300 dark:bg-darkTheme border-1 md:text-md">
-              <p className="mx-auto">3rd Jan</p>
+              <p className="mx-auto">{getDayFormat(val.startDate)}</p>
             </div>
             <div className="flex items-center h-8 text-sm bg-gray-200 border border-gray-300 rounded-md dark:text-gray-300 dark:bg-darkTheme border-1 md:text-md">
-              <p className="mx-auto">3rd Jan</p>
+              <p className="mx-auto">{getDayFormat(val.endDate)}</p>
             </div>
             <div
               onClick={() => {
@@ -129,10 +131,10 @@ function Calendar({}: Props) {
                 setIndex(i);
               }}
               className={classNames(
-                val.label === "Vacation" && "bg-green-200",
-                val.label === "Examination" && "bg-red-200",
-                val.label === "Mid exam" && "bg-red-200",
-                val.label === "Study leave" && "bg-orange-200",
+                val.label === "Vacation" && "bg-green-200 dark:bg-green-400 dark:text-black ",
+                val.label === "Examination" && "bg-red-200 dark:bg-red-400 dark:text-black ",
+                val.label === "Mid exam" && "bg-red-200 dark:bg-red-400 dark:text-black ",
+                val.label === "Study leave" && "bg-orange-200 dark:bg-orange-400 dark:text-black ",
                 "flex items-center h-8 col-span-4 bg-gray-200 border border-gray-300 rounded-md dark:text-gray-300 dark:bg-darkTheme border-1"
               )}
             >
